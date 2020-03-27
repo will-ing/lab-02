25f10a4b2c2e822cccda3ee3ae20ee3e68c5987e
will-ing <willkoger@gmail.com>
2020-03-26 21:19:12 -0700

    filter box has ooption, selection is broken

--- js/app.js
+++ js/app.js
@@ -1,19 +1,14 @@
 'use strict'
 
 //Feature #1: Display images
+const hornStorage = [];
+const keywordArr = ['All'];
 
-// Why are we implementing this feature?
-// As a user, I want to view the images on the page so that I can browse the photo collection.
+console.log('HORN OBJECT', hornStorage)
 
+console.log('KEYWORD ARR', keywordArr)
 
-
-// What are we going to implement?
-// Given that a user opens the application in the browser When the user navigates to the home page Then the photo gallery should display all of the images in the gallery
-
-// How are we implementing it?
-// Use AJAX, specifically $.ajax(), to read the provided JSON file.
-// Each object should become a new instance of a constructor function. Refer to the data to determine the necessary properties.
-// Use jQuery to make a copy of the HTML template of the photo component. For each object, fill in the duplicated template with its properties, then append the copy to the DOM.
+//renders images on html
 constructHorn.prototype.render = function (){
   const tpl = $('#photo-template').html();
   const $createSection = $('<section></section>');
@@ -22,7 +17,7 @@ constructHorn.prototype.render = function (){
   $createSection.find('img').attr('src', this.image);
   $createSection.find('p').text(this.description);
   $('main').append($createSection)
-} // got help from Jesse Pena
+} // got help from Jesse Pena. The key was to use .html
 
 
 
@@ -30,15 +25,15 @@ $.ajax('data/page-1.json', {METHOD: 'GET', DATATYPE: 'JSON'})
 .then(hornData => {
   hornData.forEach(hornType => {
     new constructHorn(hornType).render();
+    if(!keywordArr.includes(hornType.keyword)){keywordArr.push(hornType.keyword)};
+    
   })
+  filterBox();
+  choices();
 })
 
-const hornStorage = [];
-console.log('HORN OBJECT', hornStorage)
 
-console.log('THIS ONE', )
-
-
+// constructs images and stores them in a arr
 function constructHorn(eachOne){
   this.image = eachOne.image_url;
   this.title = eachOne.title;
@@ -48,14 +43,36 @@ function constructHorn(eachOne){
   hornStorage.push(this);
 }
 
-
 // Feature #2: Filter images
 
 // Why are we implementing this feature?
 // As a user, I want to be able to filter the images so that I can view only images that match a keyword.
 
+// shows list in drop down box
+function filterBox() {
+  let select = $('#dropdown');
+  select.empty();
+  keywordArr.forEach( word => {
+    let $options = $(`<option value=${word}>${word}</option>`);
+    select.append($options);
+  })
+}
+
 // What are we going to implement?
 // Given that a user clicks on the dropdown menu When the user selects one of the options Then only the images whose keyword matches the option should be displayed
+
+// this broken
+const choices = () => {
+  $('select').on('change', function(){
+    if(this.value !== 'All'){
+      $('section').hide();
+      $(`value=${this.value}`).show();
+    }
+  })
+}
+
+
+
 
 // How are we implementing it?
 // Create a <select> element which contains unique <option> elements extracted dynamically from the JSON file, one for each keyword.
